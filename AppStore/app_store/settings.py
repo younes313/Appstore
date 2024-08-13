52796387f394ab46cdd2436ce2eb844660fbea22
Unes <younes.moradi@tapsi.cab>
2024-08-14 02:44:49 +0330

    add purchase app & model

--- AppStore/app_store/settings.py
+++ AppStore/app_store/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'user_management',
     'app',
     'wallet',
+    'purchase',
 ]
 
 MIDDLEWARE = [
---
+++ AppStore/purchase/models/model_purchase.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.crypto import get_random_string
+
+from app.models import App
+
+
+class Purchase(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    app = models.ForeignKey(App, on_delete=models.CASCADE)
+    unique_key = models.CharField(max_length=32, unique=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.unique_key:
+            self.unique_key = self.generate_unique_key()
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_unique_key():
+        return get_random_string(32)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'app'], name='unique_purchase')
+        ]
